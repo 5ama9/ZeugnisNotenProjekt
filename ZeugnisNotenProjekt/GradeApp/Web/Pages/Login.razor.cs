1936189ef9f8322d542126545de11c57c4b13c11
5ama9 <amueller10@online.gibz.ch>
2025-12-11 16:01:49 +0100

    Add files via upload

---
+++ ZeugnisNotenProjekt/GradeApp/Web/Pages/Login.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Web.Services;
+using GradeApp.Shared.DTOs.Request;
+
+namespace Web.Pages;
+
+public partial class Login
+{
+    [Inject] private IAuthService _authSerivce { get; set; }
+    [Inject] private IJSRuntime JSRuntime { get; set; }
+
+    private UserLoginRequestDto loginModel = new();
+
+    private string _errorMessage;
+    private string _successMessage;
+
+    private async Task HandleLogin()
+    {
+        var response = await _authSerivce.LoginAsync(loginModel);
+        if (response != null)
+        {
+            await JSRuntime.InvokeVoidAsync("localStorage.setItem", "jwtToken", response.Token);
+            _successMessage = "Login successful.";
+        }
+        else
+        {
+            _errorMessage = "Login failed. Please check credentials.";
+        }
+    }
+}
